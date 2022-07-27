1069ee4f17ae5d50d84d5cd6b17832ffe3ca874b
Sergei Mellow <sergei.mellow@gmail.com>
2022-07-27 13:23:04 -0400

    a pass at top-k-frequent-elements, problem 347

--- Leet0347.java
+++ Leet0347.java
@@ -1,19 +1,46 @@
-//  https://leetcode.com/problems/top-k-frequent-elements/
+// https://leetcode.com/problems/top-k-frequent-elements/
+// Runtime: 19 ms, faster than 52.74% of Java online submissions for Top K Frequent Elements.
+// Memory Usage: 49.7 MB, less than 80.01% of Java online submissions for Top K Frequent Elements.
+import java.util.*;
+import java.util.stream.Collectors;
 
-import java.util.Arrays;
 
 public class Leet0347 {
 
     public static void main(String[] args) {
 
-        int[] input = {};
+        int[] input = {1,1,1,2,2,3};
         int k = 2;
         System.out.println(Arrays.toString(topKFrequent(input, k)));
     }
 
     static public int[] topKFrequent(int[] nums, int k) {
-        int result[] = {};
-        // WIP
+        Map<Integer, Integer> integerCountByIntegerValue = new HashMap<>();
+
+        for (int num : nums)
+        {
+            integerCountByIntegerValue.merge(num, 1, Integer::sum);
+        }
+
+        Set<Integer> topKIntegerCounts = integerCountByIntegerValue.values()
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(k)
+                .collect(Collectors.toSet());
+
+        // build the result using the topKIntegerCounts by searching for them in integerCountByIntegerValue map
+        int i = 0;
+        int[] result = new int[k];
+        for(Integer key : integerCountByIntegerValue.keySet()) {
+            if (i == k)
+            {
+                break;
+            }
+            if (topKIntegerCounts.contains(integerCountByIntegerValue.get(key))) {
+                result[i] = key;
+                i++;
+            }
+        }
 
         return result;
     }
