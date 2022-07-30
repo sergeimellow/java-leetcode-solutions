c6855d6c94754c66dccdac416a8a184852a08bd3
Sergei Mellow <sergei.mellow@gmail.com>
2022-07-29 20:23:09 -0400

    played around with 206, reversing linked lists

---
+++ Leet0206.java
@@ -0,0 +1,61 @@
+public class Leet0206 {
+    public static void main(String[] args) {;
+
+        ListNode nodeOne = new ListNode(1);
+        ListNode nodeTwo = new ListNode(2);
+        ListNode nodeThree = new ListNode(3);
+        ListNode nodeFour = new ListNode(4);
+        nodeOne.next = nodeTwo;
+        nodeTwo.next = nodeThree;
+        nodeThree.next = nodeFour;
+
+        ListNode reversedLinkedList = reverseList(nodeOne);
+        String a = "ANC";
+        System.out.println(reversedLinkedList);
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+//    public static ListNode reverseList(ListNode head) {
+//        return reverseList(null, head);
+//    }
+
+    public static ListNode reverseList(ListNode head)
+    {
+        if (head==null)
+        {
+            return null;
+        }
+
+        ListNode prev = null;
+        ListNode curNode = head;
+        ListNode next;
+
+        while (curNode != null)
+        {
+            next = curNode.next;
+            curNode.next = prev;
+            prev = curNode;
+            curNode = next;
+
+        }
+
+        return prev;
+    }
+
+    private static ListNode reverseList(ListNode prev,ListNode curNode){
+        if(curNode==null)
+        {
+            return prev;
+        }
+        ListNode next = curNode.next;
+        curNode.next = prev;
+        return reverseList(curNode,next);
+    }
+}
