b976c1a5a0ee2d7a51299c8202a368eccafb1ce6
Sergei Mellow <sergei.mellow@gmail.com>
2022-07-31 00:54:20 -0400

    Problem 2013

---
+++ Leet2013.java
@@ -0,0 +1,106 @@
+// https://leetcode.com/submissions/detail/761120641/
+public class Leet2013 {
+//    private Map<String, Integer> pointCountByXYPosition;
+    private int maxRow = 0;
+    private int maxCol = 0;
+    private Integer[][] matrix;
+
+    private static final String COUNT_COMMAND = "count";
+    private static final String ADD_COMMAND = "add";
+    private static final String DETECT_SQUARES_COMMAND = "DetectSquares";
+
+    public Leet2013(String[] commands, int[][] commandInputs)
+    {
+        for (int i = 0; i < commands.length; i++) {
+           if(commands[i].equals(ADD_COMMAND)) {
+               maxRow = Math.max(maxRow, commandInputs[i][0]);
+               maxCol = Math.max(maxCol, commandInputs[i][1]);
+            }
+        }
+        // "0, 0" -> null means 0 points at index 0 of the matrix
+        // "1, 3' -> 2 means in the second row and the 4th columns there are 2 points
+        matrix = new Integer[maxRow+1][maxCol+1];
+        //matrix = new Integer[1001][1001];
+
+        Integer[] output = new Integer[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+
+            if (commands[i].equals(COUNT_COMMAND)) {
+                output[i] = count(commandInputs[i]);
+            }
+            else if(commands[i].equals(ADD_COMMAND)) {
+                add(commandInputs[i]);
+                output[i] = null;
+            }
+            else if(commands[i].equals(DETECT_SQUARES_COMMAND)) {
+                output[i] = null;
+            }
+        }
+    }
+
+    public void add(int[] point) {
+        if (matrix[point[0]][point[1]] == null) {
+            matrix[point[0]][point[1]] = 1;
+        }
+        else {
+            matrix[point[0]][point[1]] = matrix[point[0]][point[1]] + 1;
+        }
+    }
+
+    // starts the detected amount of squares  this point can form
+    public int count(int[] point) {
+        int currentSquareCount = 0;
+        int currentPointCount = 1;
+        int startRow = point[0];
+        int startCol = point[1];
+        int curRow = point[0];
+        int curCol = point[1];
+
+
+
+        while (curRow != 0 && curCol != 0 )
+        {
+            curRow--;
+            curCol--;
+            if (matrix[curRow][curCol] != null && matrix[startRow][curCol] != null && matrix[curRow][startCol] != null) {
+                currentSquareCount = currentSquareCount + (currentPointCount * matrix[curRow][curCol] * matrix[startRow][curCol] * matrix[curRow][startCol]);
+            }
+        }
+
+        curRow = point[0];
+        curCol = point[1];
+        while (curRow != 1000 && curCol != 1000 )
+        {
+            curRow++;
+            curCol++;
+            if (matrix[curRow][curCol] != null && matrix[startRow][curCol] != null && matrix[curRow][startCol] != null) {
+                currentSquareCount = currentSquareCount + (currentPointCount * matrix[curRow][curCol] * matrix[startRow][curCol] * matrix[curRow][startCol]);
+            }
+        }
+
+        curRow = point[0];
+        curCol = point[1];
+        while (curRow != 0 && curCol != 1000 )
+        {
+            curRow--;
+            curCol++;
+            if (matrix[curRow][curCol] != null && matrix[startRow][curCol] != null && matrix[curRow][startCol] != null) {
+                currentSquareCount = currentSquareCount + (currentPointCount * matrix[curRow][curCol] * matrix[startRow][curCol] * matrix[curRow][startCol]);
+            }
+        }
+
+        curRow = point[0];
+        curCol = point[1];
+        while (curRow != 1000 && curCol != 0 )
+        {
+            curRow++;
+            curCol--;
+            if (matrix[curRow][curCol] != null && matrix[startRow][curCol] != null && matrix[curRow][startCol] != null) {
+                currentSquareCount = currentSquareCount + (currentPointCount * matrix[curRow][curCol] * matrix[startRow][curCol] * matrix[curRow][startCol]);
+            }
+        }
+
+        return currentSquareCount;
+    }
+}
--- Scrap.java
+++ Scrap.java
@@ -1,5 +1,44 @@
+import java.util.List;
+
 public class Scrap {
     public static void main(String[] args) {
+
+//        String[] commands = {"DetectSquares", "add", "add", "add", "count", "count", "add", "count"};
+//        int[][] commandInputs = {{}, {3, 10}, {11, 2}, {3, 2}, {11, 10}, {14, 8}, {11, 2}, {11, 10}};
+//        DetectSquares obj = new DetectSquares();
+//        int[] input1 = {419,351};
+//        int[] input2 = {798,351};
+//        int[] input3 = {798,730};
+//        int[] input4 = {419,730};
+//        int[] input5 = {14,8};
+//        int[] input6 = {11,2};
+//        int[] input7 = {11,10};
+//        obj.add(input1);
+//        obj.add(input2);
+//        obj.add(input3);
+//        obj.count(input4);
+//        obj.count(input5);
+//        obj.add(input6);
+//        obj.count(input7);
+
+//        String abc = "abc";
+//["DetectSquares","add","add","add","count","add","add","add","count","add","add","add","count","add",
+//                "add","add","count","add","add","add","count","add","add","add","count","add","add","add","count","add","add","add","count","add","add","add","count","add","add","add","count","add","add","add","count"]
+//[[],[[419,351]],[[798,351]],[[798,730]],[[419,730]],[[998,1]],[[0,999]],[[998,999]],[[0,1]],[[226,561]],[[269,561]],
+//        [[226,604]],[[269,604]],[[200,274]],[[200,793]],[[719,793]],[[719,274]],[[995,99]],[[146,948]],[[146,99]],
+//        [[995,948]],[[420,16]],[[962,558]],[[420,558]],[[962,16]],[[217,833]],[[945,105]],[[217,105]],[[945,833]],
+//        [[26,977]],[[26,7]],[[996,7]],[[996,977]],[[96,38]],[[96,483]],[[541,483]],[[541,38]],[[38,924]],[[961,1]],
+//        [[961,924]],[[38,1]],[[438,609]],[[818,609]],[[818,229]],[[438,229]]]
+//["DetectSquares","add","add","add","count","count","add","count"]
+//[[],[[3,10]],[[11,2]],[[3,2]],[[11,10]],[[14,8]],[[11,2]],[[11,10]]]
+
+
+//        DetectSquares obj = new DetectSquares(commands, commandInputs);
+//        obj.add(a);
+//        Leet2013 a = new Leet2013(commands, commandInputs);
+//        String abc = "abc";
+
+
         String input =  "><<><";
         System.out.println(fixBrac(input));
     }
