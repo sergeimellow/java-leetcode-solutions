ccca45f57d80fefbd6aad43ff191667f8eae54e8
Sergei Mellow <sergei.mellow@gmail.com>
2022-08-04 11:06:40 -0400

    played around with case sens and other symbols

--- Leet0567.java
+++ Leet0567.java
@@ -6,7 +6,63 @@ public class Leet0567 {
 
 
     public static void main(String[] args) {
-        checkInclusion("ba", "123ab");
+        checkInclusionWithoutHashMapAndCaseSens("ba", "AZddabBAZaz");
+    }
+
+    public static boolean checkInclusionWithoutHashMapAndCaseSens(String s1, String s2)
+    {
+        if (s1.length() > s2.length()) return false;
+        int[] s1Arr = new int[58];
+        int[] s2Arr = new int[58];
+        int matches = 0;
+
+        for (int i = 0; i < s1.length(); i ++)
+        {
+            s1Arr[s1.charAt(i) - 'A'] += 1;
+            s2Arr[s2.charAt(i) - 'A'] += 1;
+        }
+
+        for (int i = 0; i < 58; i++)
+        {
+            if (s1Arr[i] == s2Arr[i])
+            {
+                matches += 1;
+            }
+        }
+        int index;
+        int l = 0;
+        for (int i = s1.length(); i < s2.length(); i++)
+        {
+            if (matches == 58)
+            {
+                return true;
+            }
+            index = s2.charAt(i) - 'A';
+            s2Arr[index] += 1;
+            if (s1Arr[index] == s2Arr[index])
+            {
+                matches += 1;
+            }
+            else if (s1Arr[index] + 1 == s2Arr[index])
+            {
+                matches -= 1;
+            }
+
+            index = s2.charAt(l) - 'A';
+            s2Arr[index] -= 1;
+
+            if (s1Arr[index] == s2Arr[index])
+            {
+                matches += 1;
+            }
+            else if (s1Arr[index] - 1 == s2Arr[index])
+            {
+                matches -= 1;
+            }
+
+            l += 1;
+        }
+        return matches == 58;
     }
 
 
